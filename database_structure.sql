1de860ecc3225a382cdb1acdd01b1fb923e18938
neal daniel <portsmith1212@gmail.com>
2016-07-01 12:20:52 -0400

    Added a database with test data, and redid the original db structure

--- database_structure.sql
+++ database_structure.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `filesharing` /*!40100 DEFAULT CHARACTER SET utf8
 USE `filesharing`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: filesharing
+-- Host: localhost    Database: filesharing
 -- ------------------------------------------------------
 -- Server version	5.7.13-log
 
@@ -25,26 +25,15 @@ DROP TABLE IF EXISTS `files`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `files` (
-  `fileid` int(10) unsigned NOT NULL,
+  `fileid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `userid` int(10) unsigned NOT NULL,
-  `user_file_number` int(11) NOT NULL AUTO_INCREMENT,
   `absolute_path` varchar(256) NOT NULL,
   `file_data` longblob NOT NULL,
   PRIMARY KEY (`fileid`),
-  UNIQUE KEY `userfilenumber_UNIQUE` (`user_file_number`),
   KEY `files_user_fk_idx` (`userid`),
   CONSTRAINT `files_user_fk` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `files`
---
-
-LOCK TABLES `files` WRITE;
-/*!40000 ALTER TABLE `files` DISABLE KEYS */;
-/*!40000 ALTER TABLE `files` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -60,17 +49,12 @@ CREATE TABLE `user` (
   PRIMARY KEY (`userid`),
   UNIQUE KEY `userid_UNIQUE` (`userid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping routines for database 'filesharing'
 --
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +65,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-30  9:35:31
+-- Dump completed on 2016-07-01 12:19:44
